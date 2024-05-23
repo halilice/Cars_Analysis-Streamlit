a80afd9f510c839368c8adc9d1afe2587fbeb772
halilice <169132075+halilice@users.noreply.github.com>
2024-05-23 13:12:49 +0200

    Delete my_streamlit_app.py

--- my_streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title('Hello Wilders, welcome to my application!')
-
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
-
